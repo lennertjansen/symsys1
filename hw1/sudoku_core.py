0a6a2d1d46a342beaa6e62b7a3930c5956c8df52
lennertjansen <lennertjansen95@gmail.com>
2020-06-04 20:36:31 +0200

    First attempts at sudoku solver with propagation (lone singles)

--- hw1/sudoku_core.py
+++ hw1/sudoku_core.py
@@ -2,6 +2,40 @@
 ### Propagation function to be used in the recursive sudoku solver
 ###
 def propagate(sudoku_possible_values,k):
+
+    print(sudoku_possible_values)
+
+    # check rows for naked pairs
+    # for row in sudoku_possible_values:
+    #     print(row)
+    #     for possible_row_values in row:
+    #         naked_pair = False
+    #         possible_values_to_remove = []
+    #         if len(possible_row_values) == 2:
+    #             if row.count(possible_row_values) > 1:
+    #                 print('Naked stuff found')
+    #                 naked_pair = True
+    #                 possible_values_to_remove.append(possible_row_values)
+
+    # simplest/naive propagation strategy: every time you encounter a single
+    # possible value in a unit, remove it from all other possible values in the unit
+    for row in sudoku_possible_values:
+        print(row)
+        lone_singles = [value[0] for value in row if len(value) == 1]
+        print(lone_singles)
+
+        for possible_row_values in row:
+            if len(possible_row_values) > 1:
+                print('Hier')
+                # print(possible_row_values)
+                possible_row_values_update = [value for value in possible_row_values if value not in lone_singles]
+                possible_row_values = possible_row_values_update
+                # print(possible_row_values)
+
+        print(row)
+
+
+
     return sudoku_possible_values;
 
 ###
