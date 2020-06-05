8c3c85d9912a126e161a8ee6ce95cfcdfe393ee7
lennertjansen <lennertjansen95@gmail.com>
2020-06-05 19:13:03 +0200

    Succesfully implemented unit-wise propagation by removing lone singles from all rows, columns and houses.

--- hw1/sudoku_core.py
+++ hw1/sudoku_core.py
@@ -3,8 +3,6 @@
 ###
 def propagate(sudoku_possible_values,k):
 
-    print(sudoku_possible_values)
-
     # check rows for naked pairs
     # for row in sudoku_possible_values:
     #     print(row)
@@ -19,20 +17,56 @@ def propagate(sudoku_possible_values,k):
 
     # simplest/naive propagation strategy: every time you encounter a single
     # possible value in a unit, remove it from all other possible values in the unit
+
+    # row-wise removal of lone singles
     for row in sudoku_possible_values:
-        print(row)
         lone_singles = [value[0] for value in row if len(value) == 1]
-        print(lone_singles)
-
-        for possible_row_values in row:
+        for i, possible_row_values in enumerate(row):
             if len(possible_row_values) > 1:
-                print('Hier')
-                # print(possible_row_values)
                 possible_row_values_update = [value for value in possible_row_values if value not in lone_singles]
-                possible_row_values = possible_row_values_update
-                # print(possible_row_values)
+                row[i] = possible_row_values_update
+    # #
+    # # # column-wise removal of lone singles
+    # # # print('Before')
+    # # # print(sudoku_possible_values)
+    for col_index in range(k ** 2):
+        # TODO: see if you can speed this up, this seems excessive
+        certain_col_values = []
+        for row_index in range(k ** 2):
+            if len(sudoku_possible_values[row_index][col_index]) == 1:
+                certain_col_values.append(sudoku_possible_values[row_index][col_index][0])
 
-        print(row)
+        for row_index in range(k ** 2):
+            if len(sudoku_possible_values[row_index][col_index]) > 1:
+                # print('My man')
+                possible_col_values_update = [value for value in sudoku_possible_values[row_index][col_index] if value not in certain_col_values]
+                sudoku_possible_values[row_index][col_index] = possible_col_values_update
+
+    # block-wise removal of certain values
+    for i1 in range(k):
+        for j1 in range(k):
+            certain_block_values = []
+            for i2 in range(k):
+                for j2 in range(k):
+                    row_index = i1 * k + i2
+                    col_index = j1 * k + j2
+
+                    possible_block_values = sudoku_possible_values[row_index][col_index]
+                    if len(possible_block_values) == 1:
+                        certain_block_values.append(possible_block_values[0])
+
+            for i2 in range(k):
+                for j2 in range(k):
+                    row_index = i1 * k + i2
+                    col_index = j1 * k + j2
+
+                    possible_block_values = sudoku_possible_values[row_index][col_index]
+                    if len(possible_block_values) > 1:
+                        possible_block_values_update = [value for value in sudoku_possible_values[row_index][col_index] if value not in certain_block_values]
+                        sudoku_possible_values[row_index][col_index] = possible_block_values_update
+
+
+
 
 
 
